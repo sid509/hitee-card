921ddff94cb4fad9053ca26308340ba4e415bec9
Sudip Dahal <sudipdahal509@gmail.com>
2026-04-17 10:38:31 +0545

    Globalize DataTables and synchronize script loading to fix .DataTable is not a function error

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,9 +12,9 @@ window.bootstrap = bootstrap;
 import PerfectScrollbar from 'perfect-scrollbar';
 window.PerfectScrollbar = PerfectScrollbar;
 
-import DataTable from 'datatables.net-bs5';
-import 'datatables.net-responsive-bs5';
-window.DataTable = DataTable;
+// import DataTable from 'datatables.net-bs5';
+// import 'datatables.net-responsive-bs5';
+// window.DataTable = DataTable;
 
 // Helpers
 import './hitee/helpers';
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,10 +24,18 @@
 
     <!-- Core JS -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js" defer></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js" defer></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js" defer></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js" defer></script>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" defer></script>
 
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+    <!-- DataTables CSS -->
+    <link href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css" rel="stylesheet" />
 
     <!-- Select2 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
