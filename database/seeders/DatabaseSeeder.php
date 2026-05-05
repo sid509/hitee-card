6a40b4d0af57edd6615cebee37fe6678059bb3d2
Sudip Dahal <sudipdahal509@gmail.com>
2026-05-06 00:53:39 +0545

    chore: test ad

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,19 +19,19 @@ class DatabaseSeeder extends Seeder
             PermissionSeeder::class,
             RoleSeeder::class,
             UserSeeder::class,
-            
+
             // 1. COMPREHENSIVE DATA INGESTION
             // This replaces individual company seeders with the full JSON dataset
             JsonDataSeeder::class,
-            
+
             // 2. Create Buses and link to Routes
             BusSeeder::class,
-            
+
             // 3. Create other assets
             ParkingSeeder::class,
             ParkingAttributeSeeder::class,
             StaffSeeder::class,
-            
+
             // 4. Final Step: Simulation (requires Buses with Fares and Customers with Cards)
             CardSeeder::class,
             UltraRealisticTransitSeeder::class,
